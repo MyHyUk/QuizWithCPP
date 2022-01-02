ee66f817752d77097f7ec5a81676b29b774bc0d2
MyHyUk <h_u_kwon@kakao.com>
2022-01-02 19:03:45 +0900

    Initial Commit

---
+++ QuizWithCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  QuizWithCPP
+//
+//  Created by 형욱 on 2022/01/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
